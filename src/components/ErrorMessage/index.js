87eefa7866b5b103c1a8064e6f4be82379e1d657
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-12 10:27:39 -0300

    fix: show message in the label

--- src/components/ErrorMessage/index.js
+++ src/components/ErrorMessage/index.js
@@ -2,7 +2,9 @@ import './styles.css';
 
 const ErrorMessage = (message) => {
     return (
-        <label htmlFor="" className='error__message'>{message}</label>
+        <>
+            <label htmlFor="" className='error__message'>{message.message}</label>
+        </>
     );
 }
 
