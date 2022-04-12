b6fc78c87d3cd24453e0739ce0410971e1ce3423
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-12 09:27:21 -0300

    feat: date and time storage

--- src/components/FormRegister/index.js
+++ src/components/FormRegister/index.js
@@ -1,18 +1,19 @@
 import './styles.css';
 import { useState } from 'react';
-import { getDay, getMonth, getYear } from 'date-fns';
+import { getHours, getDay, getDate, getMonth, getYear } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const FormRegister = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    const [startTime, setStartTime] = useState(new Date(2022, 5, 11, 7));
+    const [birthDate, setBirthDate] = useState(new Date());
+    const [dateAppointment, setDateAppointment] = useState(new Date());
+    const [timeAppointment, setTimeAppointment] = useState(new Date(2022, 5, 11, 8));
     const [newAppointment, setNewAppointment] = useState({
         name: '',
-        birth_date: '',
-        day: '',
-        time: ''
+        birth_date: [ getDate(birthDate), (getMonth(birthDate)+1) , getYear(birthDate) ].join('/'),
+        date_appointment: [ getDate(dateAppointment), (getMonth(dateAppointment)+1) , getYear(dateAppointment) ].join('/'),
+        time_appointment: getHours(timeAppointment)
     });
 
     const isWeekday = (date) => {
@@ -20,23 +21,39 @@ const FormRegister = () => {
         return day !== 0 && day !== 6;
     }
 
-    const handleChange = (event) => {
+    const handleChange = (event, name) => {
+        if(name === 'name') {
+            setNewAppointment({
+                ...newAppointment,
+                name: event.target.value
+            });
+            return;
+        }
+
+        if(name === 'time_appointment') {
+            setNewAppointment({
+                ...newAppointment,
+                time_appointment: getHours(event)
+            });
+            return;
+        }
+
         setNewAppointment({
             ...newAppointment,
-            [event.target.name]: event.target.value
-        });
+            [name]: [ getDate(event), (getMonth(event)+1), getYear(event) ].join('/')
+        })
     }
 
     const handleSubmit = () => {
+        
         console.log(newAppointment);
-        console.log(startDate, startTime)
     }
 
     return (
         <form action="" className='form__container' onSubmit={e => e.preventDefault()}>
             <div className="form__field">
                 <label htmlFor="name" className='form__label'>Nome</label>
-                <input type="text" id='name' name='name' className='form__input' onChange={e => handleChange(e)} />
+                <input type="text" id='name' className='form__input' onChange={e => handleChange(e, 'name')} />
             </div>
             <div className="form__field">
                 <label htmlFor="birth__date" className='form__label'>Data de nascimento</label>
@@ -47,41 +64,39 @@ const FormRegister = () => {
                     locale={pt}
                     dateFormat="dd/MM/yyyy"
                     maxDate={new Date()}
-                    selected={startDate} 
-                    onChange={(date) => setStartDate(date)}
+                    selected={birthDate} 
+                    onChange={e => { setBirthDate(e); handleChange(e, 'birth_date') }}
                     fixedHeight
                 />
             </div>
             <div className="form__field">
-                <label htmlFor="" className='form__label'>Dia do agendamento</label>
+                <label htmlFor="date_appointment" className='form__label'>Dia do agendamento</label>
                 <DatePicker
                     className='form__datepicker'
-                    id='day'
-                    name='day'
+                    id='date_appointment'
                     locale={pt}
                     dateFormat="dd/MM/yyyy"
                     minDate={new Date()}
                     filterDate={isWeekday}
-                    selected={startDate} 
-                    onChange={(date) => setStartDate(date)}
+                    selected={dateAppointment} 
+                    onChange={e => { setDateAppointment(e); handleChange(e, 'date_appointment') }}
                     fixedHeight 
                 />
             </div>
             <div className="form__field">
-                <label htmlFor="" className='form__label'>Horário do agendamento</label>
+                <label htmlFor="time_appointment" className='form__label'>Horário do agendamento</label>
                 <DatePicker
-                    selected={startTime}
-                    onChange={(date) => setStartTime(date)}
+                    selected={timeAppointment}
+                    onChange={e => { setTimeAppointment(e); handleChange(e, 'time_appointment') }}
                     className='form__datepicker'
-                    id='time'
-                    name='time'
+                    id='time_appointment'
                     showTimeSelect
                     showTimeSelectOnly
                     timeIntervals={60}
                     timeCaption="Horário"
                     timeFormat="HH:mm"
                     dateFormat="HH:mm"
-                    minTime={(new Date(2022, 0, 1, 7))}
+                    minTime={(new Date(2022, 0, 1, 8))}
                     maxTime={(new Date(2022, 0, 1, 17))}
                 />
             </div>
