d35deabce3aea1e8a6c24887376b42acfd4e8209
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-16 19:44:09 -0300

    feat: portal routes config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Dashboard from './pages/dashboard';
 import RegisterAppointment from './pages/registerAppointment';
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <RegisterAppointment />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/register' element={<RegisterAppointment />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
