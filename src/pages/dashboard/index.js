fb206f5d7f0d698002516f2f039e9286d8c6b8c4
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-16 20:18:21 -0300

    feat: import search bar component and style the container

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,11 +1,13 @@
 import DashboardTable from '../../components/DashboardTable';
 import './styles.css';
 import ButtonNewAppointment from '../../components/ButtonNewAppointment';
+import DashboardSearchBar from '../../components/DashboardSearchBar';
 
 const Dashboard = () => {    
     return (
         <div className="dashboard__container">
             <div className="dashboard__header">
+                <DashboardSearchBar />
                 <ButtonNewAppointment />
             </div>
             <div className="dashboard__body">
