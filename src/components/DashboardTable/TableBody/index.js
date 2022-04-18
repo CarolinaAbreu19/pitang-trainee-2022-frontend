6645a724ec5bebf48d39ac76c1402a44561179b4
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-18 16:27:50 -0300

    fix: correct div children HTML tags

--- src/components/DashboardTable/TableBody/index.js
+++ src/components/DashboardTable/TableBody/index.js
@@ -1,12 +1,12 @@
 import './styles.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import button__edit from '../../../assets/button_edit_table.svg';
+import { useContext, useEffect, useState } from 'react';
 import button__delete from '../../../assets/button_delete_table.svg';
+import button__attend from '../../../assets/button_attend_table.svg';
 
 const TableBody = () => {
 
     const [appointmentsData, setAppointmentsData] = useState([]);
+    const [deleteAppointment, setDeleteAppointment] = useState(false);
 
     useEffect(() => {
         try {
@@ -23,24 +23,28 @@ const TableBody = () => {
         }
     }, []);
 
+    function handleDelete() {
+        !deleteAppointment ? setDeleteAppointment(true) : setDeleteAppointment(false);
+    }
+
 
     return (
-        <tbody className="table__body">
+        <div className="table__body">
             { appointmentsData.map((appointment) => (
-                <tr className='table__row' key={appointment.id}>
-                    <td className='table__element --id'>{appointment.id}</td>
-                    <td className='table__element'>{appointment.name}</td>
-                    <td className='table__element'>{appointment.birth_date}</td>
-                    <td className='table__element'>{appointment.date_appointment}</td>
-                    <td className='table__element'>{appointment.time_appointment}h</td>
-                    <td className={`table__element ${appointment.situation === 'waiting' ? '--waiting' : '--done'}`}>{appointment.situation === 'waiting' ? 'Em espera' : 'Atendido'}</td>
-                    <td className='table__element action__buttons'>
-                        <button className='table__button'><img src={button__edit} alt="Editar agendamento" /></button>
-                        <button className='table__button'><img src={button__delete} alt="Excluir agendamento" /></button>
-                    </td>
-                </tr>
+                <ul className='table__row' key={appointment.id}>
+                    <li className='table__element --id'>{appointment.id}</li>
+                    <li className='table__element'>{appointment.name}</li>
+                    <li className='table__element'>{appointment.birth_date}</li>
+                    <li className='table__element'>{appointment.date_appointment}</li>
+                    <li className='table__element'>{appointment.time_appointment}h</li>
+                    <li className={`table__element ${appointment.situation === 'waiting' ? '--waiting' : '--done'}`}>{appointment.situation === 'waiting' ? 'Em espera' : 'Atendido'}</li>
+                    <li className='table__element action__buttons'>
+                        <button className='table__button --attend'><img src={button__attend} alt="Realizar atendimento" /></button>
+                        <button className='table__button --delete' onClick={() => handleDelete()}><img src={button__delete} alt="Cancelar agendamento" /></button>
+                    </li>
+                </ul>
             ))}
-        </tbody>
+        </div>
     );
 }
 
