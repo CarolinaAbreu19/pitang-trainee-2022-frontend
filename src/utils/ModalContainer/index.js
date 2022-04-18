ab3ae19b885e7a5a2759acc787eae14b892a5c53
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-18 19:54:25 -0300

    fix: remove exit button of modal component

--- src/utils/ModalContainer/index.js
+++ src/utils/ModalContainer/index.js
@@ -4,9 +4,6 @@ const ModalContainer = (props) => {
     return (
         <div className="modal__background">
             <div className="modal__container">
-                <div className="modal__hide">
-                    <button>X</button>
-                </div>
                 {props.children}
             </div>
         </div>
