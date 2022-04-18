982f2c2b4810ca3518a40d54e1558137b6dbfbb9
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-18 08:49:28 -0300

    feat: config context tree

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import Dashboard from './pages/dashboard';
 import RegisterAppointment from './pages/registerAppointment';
 import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import Context from './contexts/Context';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/register' element={<RegisterAppointment />} />
-        </Routes>
-      </BrowserRouter>
+      <Context.Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/register' element={<RegisterAppointment />} />
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
     </div>
   );
 }
