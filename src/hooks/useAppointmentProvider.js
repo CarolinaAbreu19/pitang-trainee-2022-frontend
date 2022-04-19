48c9e67c6af4c6251847cf8e86016ee1228f8f05
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-19 08:36:22 -0300

    feat: hook functions loadAppointments and filterAppointments

--- src/hooks/useAppointmentProvider.js
+++ src/hooks/useAppointmentProvider.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const useAppointmentProvider = () => {
     const [modalFilterDate, setModalFilterDate] = useState(false);
     const [modalFilterTime, setModalFilterTime] = useState(false);
+    const [appointmentsData, setAppointmentsData] = useState([]);
 
     const toggleModalFilterDate = () => {
         modalFilterDate ? setModalFilterDate(false) : setModalFilterDate(true);
@@ -11,13 +12,36 @@ const useAppointmentProvider = () => {
         modalFilterTime ? setModalFilterTime(false) : setModalFilterTime(true);
     }
 
+    const loadAppointments = async () => {
+        const response = await fetch('http://localhost:3333/appointment', {
+            method: 'GET'
+        });
+        const data = await response.json();
+        setAppointmentsData(data.appointments);
+    }
+
+    const filterAppointments = async (filter, value) => {
+        const response = await fetch(`http://localhost:3000/appointments?filter=${filter}&value=${value}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        setAppointmentsData(data.appointments);
+    }
+
     return {
         modalFilterDate,
         setModalFilterDate,
-        toggleModalFilterDate,
         modalFilterTime,
         setModalFilterTime,
-        toggleModalFilterTime
+        appointmentsData,
+        setAppointmentsData,
+        toggleModalFilterDate,
+        toggleModalFilterTime,
+        loadAppointments,
+        filterAppointments
     }
 }
 
