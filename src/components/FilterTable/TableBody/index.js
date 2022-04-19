fe46ff098f163d90d47534de1ac7ae60575e80c0
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-19 17:19:30 -0300

    feat: create FilterTable component

---
+++ src/components/FilterTable/TableBody/index.js
@@ -0,0 +1,40 @@
+import './styles.css';
+import { useEffect, useState } from 'react';
+import button__delete from '../../../assets/button_delete_table.svg';
+import button__attend from '../../../assets/button_attend_table.svg';
+import useAppointmentProvider from '../../../hooks/useAppointmentProvider';
+
+const TableBody = (props) => {
+    const [deleteAppointment, setDeleteAppointment] = useState(false);
+    const { appointmentsData, loadAppointments, filterData, setFilterData } = useAppointmentProvider();
+
+    useEffect(() => {
+        console.log(filterData);
+    }, []);
+
+    function handleDelete() {
+        !deleteAppointment ? setDeleteAppointment(true) : setDeleteAppointment(false);
+    }
+
+    return (
+        <div className="table__body">
+            { props.list.map((appointment) => (
+                    <ul className='table__row' key={appointment.id}>
+                        <li className='table__element --id'>{appointment.id}</li>
+                        <li className='table__element'>{appointment.name}</li>
+                        <li className='table__element'>{appointment.birth_date}</li>
+                        <li className='table__element'>{appointment.date_appointment}</li>
+                        <li className='table__element'>{appointment.time_appointment}h</li>
+                        <li className={`table__element ${appointment.situation === 'waiting' ? '--waiting' : '--done'}`}>{appointment.situation === 'waiting' ? 'Em espera' : 'Atendido'}</li>
+                        <li className='table__element action__buttons'>
+                            <button className='table__button --attend'><img src={button__attend} alt="Realizar atendimento" /></button>
+                            <button className='table__button --delete' onClick={() => handleDelete()}><img src={button__delete} alt="Cancelar agendamento" /></button>
+                        </li>
+                    </ul>
+                ))
+            }
+        </div>
+    );
+}
+
+export default TableBody;
---
+++ src/components/FilterTable/TableHead/index.js
@@ -0,0 +1,19 @@
+import './styles.css';
+
+const TableHead = () => {
+    return (
+        <div className='table__head'>
+            <ul className='table__row'>
+                <li className='table__head__element'>ID</li>
+                <li className='table__head__element'>Nome</li>
+                <li className='table__head__element'>Data de nascimento</li>
+                <li className='table__head__element'>Dia do agendamento</li>
+                <li className='table__head__element'>Horário do agendamento</li>
+                <li className='table__head__element'>Situação</li>
+                <li className='table__head__element'>Ações</li>
+            </ul>
+        </div>
+    );
+}
+
+export default TableHead;
