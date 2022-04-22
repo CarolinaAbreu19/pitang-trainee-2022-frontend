6ad94d0d3fe4e19f16524fcef70b310ef02eabbe
Carolina Abreu <anacarolinaks19@gmail.com>
2022-04-22 09:47:33 -0300

    refactor: rename props to 'alertType'

--- src/utils/AlertMessage/index.js
+++ src/utils/AlertMessage/index.js
@@ -4,9 +4,9 @@ import error from '../../assets/icon_alert_error.svg';
 
 const AlertMessage = (props) => {
     return (
-        <div className={`alert__container ${props.alert === 'success' ? '--success' : '--error'}`}>
+        <div className={`alert__container ${props.alertType === 'success' ? '--success' : '--error'}`}>
             <div className='alert__message'>
-                <img src={props.alert === 'success' ? success : error} alt="Alerta" />
+                <img src={props.alertType === 'success' ? success : error} alt="Alerta" />
                 <span>{props.message}</span>
             </div>
         </div>
